94f36caccb2590c4a71c9d92a7e3206fadd4ba04
Larry Li <larryli@qq.com>
2017-11-07 00:36:10 +0800

    csrf 处理

--- JsonServer.cpp
+++ JsonServer.cpp
@@ -8,11 +8,18 @@
 AsyncCallbackWebHandler& JsonServer::onJson(const char* uri, WebRequestMethodComposite method, JsonHandler handle)
 {
   return on(uri, method, [&](AsyncWebServerRequest * request) {
+    AsyncWebHeader* referer = request->getHeader("Referer");
     AsyncResponseStream *response = request->beginResponseStream(F("text/json"));
     DynamicJsonBuffer jsonBuffer;
     JsonObject &root = jsonBuffer.createObject();
 
-    handle(request, root);
+    // csrf 检测 referer
+    if (referer != nullptr && referer->value().indexOf("://" + request->host() + "") == -1) {
+      root["ok"] = 0;
+      root["message"] = F("内部错误，请重试。");
+    } else {
+      handle(request, root);
+    }
     if (root["ok"] == 0) {
       response->setCode(422);
     }
